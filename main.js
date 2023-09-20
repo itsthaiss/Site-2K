0b7fdb9b5d560878cd161a173c17215db1c1f3ea
thais <97624303+itsthaiss@users.noreply.github.com>
2023-09-20 07:47:16 -0300

    .

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
+    //função = nome é TocaSomToim
 function TocaSomToim() {
+    //tocando o som toim
     document.querySelector('#som_tecla_toim').play();
 }
 
+//constante = nome lista 
+//tecla botões
 const Lista = document.querySelectorAll('.tecla');
 
 let Contador = 0;
 
-while (Contador < 9) {
+while (Contador < Lista.length) {
     Lista[Contador].onclick = TocaSomToim;
     Contador = Contador + 1;
     console.log(Contador);
