92b607ba99cc105e497d8b95a11d239a4f608ebe
thais <97624303+itsthaiss@users.noreply.github.com>
2023-09-27 08:17:14 -0300

    .

--- main.js
+++ main.js
@@ -12,14 +12,22 @@ const Lista = document.querySelectorAll('.tecla');
 let Contador = 0;
 
 while (Contador < Lista.length) {
-    Lista[Contador].onclick = function() {TocaSom(idSomOficial);
-}
     const classe1 = Lista[Contador]. classList[1];
     console.log(classe1);
     
     const idSomOficial = `#som_${classe1}`;
     console.log(idSomOficial);
+
+    Lista[Contador].onclick = function() {TocaSom(idSomOficial);
+}
     
 Contador = Contador + 1;
+tecla.onkeydown = function( evento) {
+   tecla.classList.add("ativa");
+   console.log(evento.code);
+}
+tecla.onkeyup = function(){
+    tecla.classList.remove("ativa");
+}
     console.log(Contador);
 }
